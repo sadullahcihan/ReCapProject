b14c193e62f7354b3f24360edb7f186bd09530d6
sadullahcihan <sado_sado1999@hotmail.com>
2022-07-22 11:55:56 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal; //Business class library is connected to data access by ICarDal Interface
+
+        //Then constructor has been created automatically by clicking the yellow lamp
+        public CarManager(ICarDal carDal) 
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars; //example DB
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car {Id=1, BrandId=1, ColorId=1, ModelYear =2009, DailyPrice=550000,Description="Honda Civic"},
+                new Car {Id=2, BrandId=1, ColorId=3, ModelYear =2010, DailyPrice=750000,Description="Honda Civic"},
+                new Car {Id=3, BrandId=2, ColorId=2, ModelYear =1985, DailyPrice=70000,Description="Tofas"},
+                new Car {Id=4, BrandId=2, ColorId=1, ModelYear =1986, DailyPrice=71000,Description="Tofas"},
+                new Car {Id=5, BrandId=3, ColorId=1, ModelYear =2022, DailyPrice=550000,Description="Mercedes"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c=>c.Id==id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c=>c.Id==car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;   
+        }
+    }
+}
